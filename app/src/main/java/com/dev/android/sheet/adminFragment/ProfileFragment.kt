51a43c171f861c8310c7b5324d70d9549da959e4
Devajit <patar1447@gmail.com>
2023-07-01 03:47:13 +0530

    User data retrieve and show in profile and add login button

--- app/src/main/java/com/dev/android/sheet/adminFragment/ProfileFragment.kt
+++ app/src/main/java/com/dev/android/sheet/adminFragment/ProfileFragment.kt
@@ -1,60 +1,106 @@
 package com.dev.android.sheet.adminFragment
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.dev.android.sheet.R
+import com.dev.android.sheet.auth.LogInActivity
+import com.dev.android.sheet.databinding.FragmentProfileBinding
+import com.dev.android.sheet.model.UserDataModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentProfileBinding
+//    private   var LOG_CODE=1
+    private val db=Firebase.database.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
+
+//   private lateinit var LogInLauncher:ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding=FragmentProfileBinding.inflate(layoutInflater)
+
+        binding.txtLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivityForResult(Intent(requireContext(),LogInActivity::class.java),LOG_CODE)
+
+        }
+
+//        LogInLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+//            if(it.resultCode==Activity.RESULT_OK){
+//                requireActivity().supportFragmentManager.beginTransaction()
+//                    .remove(this)
+//                    .commit()
+//            }
+//        }
+//
+//        binding.txtLogout.setOnClickListener {
+//            FirebaseAuth.getInstance().signOut()
+//            LogInLauncher.launch(Intent(requireContext(),LogInActivity::class.java))
+//        }
+
+
+        fetchData(binding)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun fetchData(binding: FragmentProfileBinding) {
+       val userRef=db.child("Admin")
+        userRef.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (childSnap in snapshot.children){
+                    val user=childSnap.getValue(UserDataModel::class.java)
+                    val name=user?.name
+                    val email=user?.email
+
+                    binding.profileName.text=name
+                    binding.profileEmail.text=email
                 }
             }
+
+            override fun onCancelled(error: DatabaseError) {
+                binding.profileEmail.error="something wrong"
+                binding.profileName.error="something wrong"
+            }
+
+        })
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==LOG_CODE){
+            requireActivity().supportFragmentManager.beginTransaction()
+                .remove(this)
+                .commit()
+        }
+    }
+
+    companion object{
+        private const val LOG_CODE=1001
+    }
+
 }
