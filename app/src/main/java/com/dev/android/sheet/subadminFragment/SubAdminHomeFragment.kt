376b8f94590b18269089195eff55d931e43eb5d9
Devajit <patar1447@gmail.com>
2023-07-04 18:56:46 +0530

    Subadmin data entry and employee details access given

--- app/src/main/java/com/dev/android/sheet/subadminFragment/SubAdminHomeFragment.kt
+++ app/src/main/java/com/dev/android/sheet/subadminFragment/SubAdminHomeFragment.kt
@@ -1,33 +1,25 @@
 package com.dev.android.sheet.subadminFragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dev.android.sheet.R
+import com.dev.android.sheet.admin.MainActivity
+import com.dev.android.sheet.admin.ReadActivity
+import com.dev.android.sheet.databinding.FragmentSubAdminHomeBinding
+import com.dev.android.sheet.databinding.FragmentSubAdminProfileBinding
+import com.dev.android.sheet.subadmin.EnterDataActivity
+import com.dev.android.sheet.subadmin.ReadDataActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SubAdminHomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SubAdminHomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+ private lateinit var binding:FragmentSubAdminHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +27,19 @@ class SubAdminHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sub_admin_home, container, false)
+        binding= FragmentSubAdminHomeBinding.inflate(layoutInflater)
+
+        binding.subAdminEnterData.setOnClickListener {
+            startActivity(Intent(requireContext(),MainActivity::class.java))
+        }
+        binding.subAdminReadData.setOnClickListener {
+            startActivity(Intent(requireContext(), ReadActivity::class.java))
+        }
+
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SubAdminHomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SubAdminHomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
