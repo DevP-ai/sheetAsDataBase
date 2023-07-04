f70d13e86648c49adf8a1df2f32a84ed28fb5c26
Devajit <patar1447@gmail.com>
2023-07-04 17:22:26 +0530

    Subadmin Profile data access added

--- app/src/main/java/com/dev/android/sheet/subadminFragment/SubAdminProfileFragment.kt
+++ app/src/main/java/com/dev/android/sheet/subadminFragment/SubAdminProfileFragment.kt
@@ -1,33 +1,29 @@
 package com.dev.android.sheet.subadminFragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dev.android.sheet.R
+import com.dev.android.sheet.auth.LogInActivity
+import com.dev.android.sheet.databinding.FragmentSubAdminProfileBinding
+import com.dev.android.sheet.model.UserDataModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SubAdminProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SubAdminProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+   private lateinit var binding:FragmentSubAdminProfileBinding
+   private val db=Firebase.database.reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +31,54 @@ class SubAdminProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sub_admin_profile, container, false)
+        binding= FragmentSubAdminProfileBinding.inflate(layoutInflater)
+
+
+        binding.subTxtLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivityForResult(Intent(requireContext(),LogInActivity::class.java), LOG_CODE)
+        }
+
+        fetchData(binding)
+
+        return binding.root
+    }
+
+    private fun fetchData(binding: FragmentSubAdminProfileBinding) {
+        val userRef=db.child("SubAdmin")
+        userRef.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(snap in snapshot.children){
+                    val user=snap.getValue(UserDataModel::class.java)
+                    val name=user?.name
+                    val email=user?.email
+
+                    binding.subProfileName.text=name
+                    binding.subProfileEmail.text=email
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                binding.subProfileName.error="something wrong"
+                binding.subProfileEmail.error="something wrong"
+            }
+
+        })
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode== LOG_CODE){
+            requireActivity().supportFragmentManager.beginTransaction()
+                .remove(this)
+                .commit()
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SubAdminProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SubAdminProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val LOG_CODE=1002
+
     }
 }
